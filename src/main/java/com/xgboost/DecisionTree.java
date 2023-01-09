2b7fd657f67e465d9f676bfd65fe34d59d36a257
Patrick Le <le.chi.patrick@gmail.com>
2023-01-09 14:03:45 -0600

    use record instead

--- src/main/java/com/xgboost/DecisionTree.java
+++ src/main/java/com/xgboost/DecisionTree.java
@@ -1,10 +1,5 @@
 package com.xgboost;
 
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import java.util.ArrayList;
 
 public class DecisionTree {
@@ -16,47 +11,32 @@ public class DecisionTree {
 
     public interface Node {
         boolean isLeaf();
+
         float eval(float[] featureValues);
     }
 
-    public static class Branch implements Node {
-        private int nodeId;
-        private int depth;
-        private String feature;
-        private float threshold;
-        private int leftNodeId;
-        private int rightNodeId;
-        private int missingNodeId;
-        private Node left;
-        private Node right;
+    public record Branch(int nodeId, int depth, String feature, float threshold, int leftNodeId, int rightNodeId,
+                         int missingNodeId, Node left,
+                         Node right) implements Node {
 
-        public Branch(int nodeId, int depth, String feature, float threshold, int leftNodeId, int rightNodeId, int missingNodeId, Node left, Node right){
-            this.nodeId = nodeId;
-            this.depth = depth;
-            this.feature = feature;
-            this.threshold = threshold;
-            this.leftNodeId = leftNodeId;
-            this.rightNodeId = rightNodeId;
-            this.missingNodeId = missingNodeId;
-            this.left = left;
-            this.right = right;
+        public boolean isLeaf() {
+            return false;
         }
 
-        public boolean isLeaf() {return false;}
-        public float eval(float[] featureValues) {return 0F;}
+        public float eval(float[] featureValues) {
+            return 0F;
+        }
     }
 
-    public static class Leaf implements Node {
-        private int nodeId;
-        private float leafValue;
+    public record Leaf (int nodeId, float leafValue) implements Node {
 
-        public Leaf(int nodeId, float leafValue) {
-            this.nodeId = nodeId;
-            this.leafValue = leafValue;
+        public boolean isLeaf() {
+            return true;
         }
 
-        public boolean isLeaf() {return true;}
-        public float eval(float[] featureValues) {return leafValue;}
+        public float eval(float[] featureValues) {
+            return leafValue;
+        }
 
     }
 }
--- src/test/java/com/xgboost/ModelParserTest.java
+++ src/test/java/com/xgboost/ModelParserTest.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.databind.*;
 import com.fasterxml.jackson.core.*;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class ModelParserTest {
@@ -18,11 +18,13 @@ public class ModelParserTest {
         assertEquals(myTree.trees.size(), 9);
 
         DecisionTree.Node firstTree = myTree.trees.get(0);
-        assertEquals(firstTree.isLeaf(), true);
-        assertEquals(firstTree.eval(null), -0.131447807, Math.ulp(0.01F));
+        assertTrue(firstTree.isLeaf());
+        assertEquals(firstTree.eval(null), -0.131447807, Math.ulp(0.1F));
 
         DecisionTree.Node secondTree = myTree.trees.get(1);
-        assertEquals(secondTree.isLeaf(), false);
+        assertFalse(secondTree.isLeaf());
+        DecisionTree.Branch myBranch = (DecisionTree.Branch) secondTree;
+        assertEquals(myBranch.threshold(), 8.74501896, Math.ulp(0.1F));
     }
 
 }
